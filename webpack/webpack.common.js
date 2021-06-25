bd8c142073f1971b49b3071c5f1bc54a72232b7f
Marknjo <marknjo7@gmail.com>
2021-06-25 20:30:16 +0300

    Auto read files in the pages folder and load html files nested in one directory

--- webpack/webpack.common.js
+++ webpack/webpack.common.js
@@ -1,24 +1,67 @@
 const htmlWebpackPlugin = require('html-webpack-plugin');
-const path = require('path');
 const DashboardPlugin = require('webpack-dashboard/plugin');
+const path = require('path');
+const fs = require('fs');
 
-let htmlPageNames = ['helpers/typography'];
-let multipleHtmlPlugins = htmlPageNames.map(name => {
-  let chunkName;
+const constructHtmlPageName = function () {
+  const pagesPath = path.join(process.cwd(), 'src', 'App', 'pages');
 
-  if (name.includes('/')) {
-    const path = name.split('/');
+  const filesInPagesDir = fs.readdirSync(pagesPath);
 
-    chunkName = path[name.split('/').length - 1];
-  } else {
-    chunkName = name;
+  const pagesDirs = filesInPagesDir.filter(file => !file.includes('.'));
+
+  const getHtmlFilesInADir = function (filesInPagesDir, pageDir) {
+    return filesInPagesDir
+      .filter(file => file.includes('.html'))
+      .map(htmlFile =>
+        pageDir
+          ? `${pageDir}/${htmlFile.split('.')[0]}`
+          : htmlFile.split('.')[0]
+      );
+  };
+
+  let getHtmlFilesInSubDir = [];
+
+  const htmlFilesInThePagesDir = getHtmlFilesInADir(filesInPagesDir);
+
+  if (pagesDirs.length > 0) {
+    let htmlFilesInADir = [];
+    //check if they are valid dir
+    for (const pageDir of pagesDirs) {
+      let currentPageDir = path.join(pagesPath, pageDir);
+      if (fs.existsSync(currentPageDir)) {
+        //get the html files in the path
+        const getDirFiles = fs.readdirSync(currentPageDir);
+
+        htmlFilesInADir.push(getHtmlFilesInADir(getDirFiles, pageDir));
+      }
+    }
+
+    getHtmlFilesInSubDir = htmlFilesInADir.flat();
   }
+
+  return [...getHtmlFilesInSubDir, ...htmlFilesInThePagesDir];
+};
+
+const htmlPageNames = constructHtmlPageName();
+
+con;
+
+const multipleHtmlPlugins = htmlPageNames.map(name => {
+  // let chunkName;
+
+  // if (name.includes('/')) {
+  //   const path = name.split('/');
+  //   chunkName = path[name.split('/').length - 1];
+  // } else {
+  //   chunkName = name;
+  // }
 
   return new htmlWebpackPlugin({
     template: `./src/App/pages/${name}.html`, // relative path to the HTML files
     filename: `pages/${name}.html`, // output HTML files
     inject: true,
-    chunks: ['main', chunkName], // respective JS files
+    chunks: ['main'], // respective JS files
   });
 });
 
