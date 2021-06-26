b718c91fd27ef9f8751193bc40236a01dcbb6bc7
Marknjo <marknjo7@gmail.com>
2021-06-26 14:52:12 +0300

    Fix hot relload not loading as expected - culprit watchContentBase: true

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@
 /* Base Scripts import */
 
 import './App/styles/main.scss';
+//import './App/scripts/helpers/NavLinks';
 
 //enable hot reload
 if (module.hot) {
--- src/pages/default-page/default-page.js
+++ src/pages/default-page/default-page.js
@@ -1,1 +1,6 @@
 import './styles/main.scss';
+
+//enable hot reload
+if (module.hot) {
+  module.hot.accept();
+}
